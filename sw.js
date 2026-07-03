c96617bf007fdc82da2fd21f11921106b4baf72c
John Belles <qualify155@gmail.com>
2026-07-02 22:17:08 -0400

    build: cache revisit archive codex extension

--- sw.js
+++ sw.js
@@ -26,6 +26,7 @@ const ASSETS = [
   `./js/systems/13_devtools_overlay.js?build=${BUILD_QS}`,
   `./js/systems/14_devtools_scenarios.js?build=${BUILD_QS}`,
   `./js/systems/15_devtools_balance_reports.js?build=${BUILD_QS}`,
+  `./js/systems/36_ui_revisit_archive_codex.js?build=${BUILD_QS}`,
   `./js/systems/16_relic_forge_crafting.js?build=${BUILD_QS}`,
   `./js/systems/17_relic_forge_clarity.js?build=${BUILD_QS}`,
   `./js/systems/18_relic_forge_compact_text.js?build=${BUILD_QS}`,
